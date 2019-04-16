ae8bd259ab9b9a29b8eafb47ecfdae6a6be1f243
guosanbin <guosanbin@theforceprotocol.com>
2019-04-16 21:11:23 +0800

    fix pom pkg error, add erc20 tx interface

--- src/main/java/com/theforceprotocol/blockchainrpc/ethclient/EthTransferClient.java
+++ src/main/java/com/theforceprotocol/blockchainrpc/ethclient/EthTransferClient.java
@@ -3,13 +3,17 @@ package com.theforceprotocol.blockchainrpc.ethclient;
 import com.theforceprotocol.blockchainrpc.TransactionRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.RawTransaction;
 import org.web3j.crypto.TransactionEncoder;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthGasPrice;
 import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
 import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.tx.RawTransactionManager;
 import org.web3j.tx.Transfer;
 import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.utils.Convert;
@@ -17,6 +21,11 @@ import org.web3j.utils.Numeric;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+
+import org.web3j.abi.datatypes.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * ETH转账
@@ -50,6 +59,25 @@ public class EthTransferClient {
             e.printStackTrace();
             return TransactionRecord.fail();
         }
+    }
+
+    public TransactionRecord transferERC20(String fromPrivateKey, String toAddress, String contractAddress, String amount) {
+        Credentials credentials = Credentials.create(fromPrivateKey);
+        BigDecimal value = new BigDecimal(amount);
+        try {
+            return sendERC20(credentials, contractAddress, toAddress, value);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return TransactionRecord.fail();
+        }
+    }
+
+    public static String encodeTransferData(String toAddress, BigInteger sum) {
+        Function function = new Function(
+                "transfer",  // function we're calling
+                Arrays.asList(new Address(toAddress), new Uint256(sum)),  // Parameters to pass as Solidity Types
+                Arrays.asList(new org.web3j.abi.TypeReference<Bool>() {}));
+        return FunctionEncoder.encode(function);
     }
 
     private TransactionRecord sendCoins(Credentials credentials, String toAddress, BigDecimal value) throws Exception {
@@ -89,4 +117,46 @@ public class EthTransferClient {
         }
         return TransactionRecord.success(transactionHash);
     }
+
+    private TransactionRecord sendERC20(Credentials credentials, String contractAddress, String toAddress, BigDecimal value) throws Exception {
+        Convert.Unit unit = Convert.Unit.ETHER;
+        String tokenValue = String.valueOf(value);
+        BigDecimal weiValue = Convert.toWei(tokenValue, unit);
+        if (!Numeric.isIntegerValue(weiValue)) {
+            throw new UnsupportedOperationException(
+                    "Non decimal Wei value provided: " + tokenValue + " " + unit.toString()
+                            + " = " + weiValue + " Wei");
+        }
+        EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(credentials.getAddress(), DefaultBlockParameterName.LATEST).sendAsync().get();
+        BigInteger nonce = ethGetTransactionCount.getTransactionCount();
+        System.out.println("nonce:" + nonce);
+        BigInteger sum = value.toBigIntegerExact(); // amount you want to send
+        String data = encodeTransferData(toAddress, sum);
+        BigInteger gasLimit = BigInteger.valueOf(120000); // set gas limit here
+        RawTransaction rawTransaction = RawTransaction.createTransaction(nonce, DefaultGasProvider.GAS_PRICE, gasLimit, contractAddress, data);
+
+        byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+        String hexValue = Numeric.toHexString(signedMessage);
+        System.out.println("hexValue:" + hexValue);
+
+        EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(hexValue).sendAsync().get();
+        if (ethSendTransaction == null) {
+            logger.error("没有获取到ETH交易哈希！");
+            return TransactionRecord.fail();
+        }
+        String transactionHash = ethSendTransaction.getTransactionHash();
+        logger.info("此笔转账hash地址为：" + transactionHash);
+        if (ethSendTransaction.getError() != null) {
+            if (ethSendTransaction.getError().getCode() != 1) {
+                logger.error("转账失败！");
+                logger.error("error data:" + ethSendTransaction.getError().getData());
+                logger.error("error msg:" + ethSendTransaction.getError().getMessage());
+                logger.error("error code:" + ethSendTransaction.getError().getCode());
+                logger.error(ethSendTransaction.getError().getMessage());
+                return TransactionRecord.fail(transactionHash);
+            }
+        }
+        return TransactionRecord.success(transactionHash);
+    }
+
 }
